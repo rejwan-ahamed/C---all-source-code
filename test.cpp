1a0dc836d285e4cb5e10ae5d2dcc128694325ff6
rejwan-ahamed <rejwanahamed85@gmail.com>
2023-09-14 22:17:35 +0600

    test code

---
+++ test.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+int main()
+{
+    printf("this is testing of c code in c++\n");
+
+    for (int i = 0; i < 5; i++)
+    {
+        printf("%d\n", i + 1);
+    }
+    return 0;
+}
